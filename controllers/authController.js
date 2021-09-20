2221eaee30bf69ef84bf23e7fd974e09df90d2b8
Tony Trinh <tony@bokksu.com>
2021-09-19 23:09:29 -0400

    created user model and allow sign in with jwt and hash password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,43 @@
+const jwt = require('jsonwebtoken')
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError')
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm
+  });
+
+  const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET,{
+      expiresIn: process.env.JWT_EXPIRES_IN
+  })
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser
     }
   });
 });
+
+exports.login = (req,res,next) =>{
+    const {email, password} = req.body; 
+
+    // 1) check if email and password exist 
+    if (!email || !password){
+        next(new AppError('Please provide email and password!', 400))
+    }
+
+    // 2) check if user exists and password is correct
+
+    // 3) if everything is ok, send token to client
+    const token = '';
+    res.status(200).json({
+        status: 'success',
+        token
+    })
+}
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs')
 
 //name email photo password passwordConfirm
 
@@ -24,8 +25,24 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+        validator: function(el){
+            return el === this.password
+        },
+        message: "Passwords are not the same !"
+    }
   }
 });
+
+userSchema.pre('save', async function(next){
+    //only run this function if password was modified
+    if(!this.isModified('password')) return next();
+    //hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+    //delete passwordConfim field
+    this.passwordConfirm = undefined;
+    next();
+})
 
 const User = mongoose.model('User', userSchema);
 
